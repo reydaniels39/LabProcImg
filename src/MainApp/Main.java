d7e2dde0a1d2b2139d9cc9bb2b9287564b703c57
ReyDanielS <reydaniels@outlook.com>
2023-03-22 23:17:40 -0600

    feat:(Added app window and Added a way to scale images)

---
+++ src/MainApp/Main.java
@@ -0,0 +1,10 @@
+package MainApp;
+
+public class Main {
+    static String PATH = "C:\\\\Users\\\\ReyDanielS\\\\Topico2Workspace\\\\LabProcesamientoImg\\\\imgs\\\\xd.jpg";
+    public static void main(String[] args) {
+        System.out.println("Iniciando Aplicación");
+
+        Window ventana = new Window(PATH);
+    }
+}
